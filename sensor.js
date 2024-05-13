ae0ab1f7b6c947c06ca1befc723a43c3c9b31cf3
anudeepr <anudeep.reddy1725@gmail.com>
2024-05-13 00:06:17 -0700

    Sensor ray intersection with borders

--- sensor.js
+++ sensor.js
@@ -1,20 +1,54 @@
 class Sensor {
     constructor(car) {
         this.car = car;
-        this.rayCount = 3;
-        this.rayLength = 100;
+        this.rayCount = 10;
+        this.rayLength = 150;
         this.raySpread = Math.PI/2;
         
         this.rays = [];
+        this.readings = [];
     }
 
-    update() {
+    update(roadBorders) {
+        this.#castRays();
+        this.readings = [];
+        for (let i = 0; i < this.rays.length; i++) {
+            this.readings.push(
+                this.#getReading(this.rays[i], roadBorders)
+            );
+        }
+    }
+
+    #getReading(ray, roadBorders) {
+        let touches = [];
+        for (let i = 0; i < roadBorders.length; i++) {
+            const touch = getIntersection(
+                ray[0],
+                ray[1],
+                roadBorders[i][0],
+                roadBorders[i][1]
+            );
+            if (touch) {
+                touches.push(touch)
+            }
+
+            if(touches.length == 0) {
+                return null;
+            } else {
+                const offsets = touches.map(e => e.offset);
+                const minOffset = Math.min(...offsets);
+                return touches.find(e => e.offset==minOffset);
+            }
+        }
+    }
+
+    #castRays() {
         this.rays = [];
         for (let i = 0; i < this.rayCount; i++) {
             const rayAngle = lerp(
                 this.raySpread/2,
                 -this.raySpread/2,
-                this.rayCount == 1 ? 0.5 : i/this.rayCount-1
+                this.rayCount == 1 ? 0.5 : i/(this.rayCount-1)
             ) + this.car.yaw;
 
             const start = {
@@ -31,13 +65,26 @@ class Sensor {
     }
 
     draw(ctx) {
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = "yellow";
-        this.rays.forEach(ray => {
+        for (let i = 0; i < this.rayCount; i++) {
+            let end = this.rays[i][1];
+            if (this.readings[i]) {
+                end = this.readings[i];
+            }
+            
             ctx.beginPath();
-            ctx.moveTo(ray[0].x, ray[0].y);
-            ctx.lineTo(ray[1].x, ray[1].y);
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "yellow";
+            ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
+            ctx.lineTo(end.x, end.y);
             ctx.stroke();
-        })
+
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "black";
+            ctx.moveTo(this.rays[i][1].x, this.rays[i][1].y);
+            ctx.lineTo(end.x, end.y);
+            ctx.stroke();
+        }
     }
+    
 }
