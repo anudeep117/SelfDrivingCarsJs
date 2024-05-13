61512b44d1ee63c242e70ea509b8dca3e563dab4
anudeepr <anudeep.reddy1725@gmail.com>
2024-05-13 14:45:13 -0700

    dumb neural network for controls

--- main.js
+++ main.js
@@ -3,12 +3,21 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2, canvas.width*0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50); // values in pixels
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI"); // values in pixels
+
+const traffic = [
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+];
 
 animate();
 
 function animate() {
-    car.update(road.borders);
+
+    for (let i = 0; i < traffic.length; i++) {
+        traffic[i].update(road.borders, []);
+    }
+
+    car.update(road.borders, traffic);
 
     canvas.height = window.innerHeight;
 
@@ -16,7 +25,12 @@ function animate() {
     ctx.translate(0, -car.y + canvas.height*0.7); // place the car at 70% to the bottom of road (more to see ahead)
     
     road.draw(ctx);
-    car.draw(ctx);
+    
+    traffic.forEach(trafficCar => {
+        trafficCar.draw(ctx, "red");
+    })
+    
+    car.draw(ctx, "blue");
     
     ctx.restore();
     requestAnimationFrame(animate);
